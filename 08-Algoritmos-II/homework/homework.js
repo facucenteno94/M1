3759684c8582bee3a41a2b684d88b14582a1af63
Facundo Centeno <facucenteno94@hotmail.com>
2022-11-21 23:02:19 -0300

    Homework 08 SOLVED

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -1,4 +1,5 @@
 'use strict'
+
 // No cambies los nombres de las funciones.
 
 function quickSort(array) {
@@ -6,16 +7,42 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+if (array.length <= 1) return array;
+var piv = array[Math.floor(Math.random() * array.length)];
+var izquierda = [];
+var igual = [];
+var derecha = [];
+for (let  i = 0; i < array.length; i++){
+  if (array[i]< piv){
+    izquierda.push(array[i]);
+  }else if(array[i] === piv){
+    igual.push(array[i]);
+  }else{
+    derecha.push(array[i]);
+  }
 }
+return [].concat(quickSort(izquierda),igual,quickSort(derecha));
+}
+
 
 function mergeSort(array) {
   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if (array<=1) return array;
+  var pivote = array[0];
+  var igual = [];
+  var izq = [];
+  var der = [];
+  for (let i = 0; i < array.length; i++){
+    if (array[i] < pivote) izq.push(array[i]);
+    if (array[i] === pivote) igual.push(array[i]);
+    if (array[i] > pivote) der.push(array[i]);
+  }
+  return mergeSort(izq).concat(igual).concat(mergeSort(der));
 }
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
